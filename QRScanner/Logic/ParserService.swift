c4ad37aee9b180667b924a3a69bfc1dbc0cdb3cc
Alexander Semenov <alexs@2kgroup.com>
2017-05-06 15:54:36 +0300

    working URL parsing

--- QRScanner/Logic/ParserService.swift
+++ QRScanner/Logic/ParserService.swift
@@ -19,30 +19,46 @@ class ParserService {
 
     weak var delegate: ParserServiceDelegate?
 
-    private(set) var  parsingInProgress : Bool = false
+    private(set) var parsingInProgress: Bool = false
 
     private let parsingQueue = DispatchQueue.global(qos: .userInitiated)
 
-    private let parsers : [ItemParser] = [JSONItemParser(), XMLItemParser()];
+    private let parsers: [ItemParser] = [JSONItemParser(), XMLItemParser()];
 
     func parse(code: String) {
         parsingQueue.async {
-            self.parse(code: code, usingParsers: self.parsers)
+
+            var dataToParse: String = code
+            //we can have urls in our codes - try to detect it
+            do {
+                if let codeFromUrl = try self.fetchCodeFromUrl(scannedCode: code) {
+                    dataToParse = codeFromUrl
+                }
+            } catch {
+                //we received url, but download failed
+                DispatchQueue.main.async {
+                    self.delegate?.didFinishParsing(result: ParsingResult(items: nil, error: error as NSError))
+                }
+            }
+
+            //actual parsing
+            self.parse(data: dataToParse, usingParsers: self.parsers)
         }
 
         //notify delegate, parsing have started
         DispatchQueue.main.async {
-           self.delegate?.didStartParsing(code: code)
+            self.delegate?.didStartParsing(code: code)
         }
     }
 
-    private func parse(code: String, usingParsers: [ItemParser]){
-        print("Parsing: \(code) ")
-        var parsedItems : [Item]?
+    private func parse(data: String, usingParsers: [ItemParser]) {
+        print("Parsing data: \(data) ")
+        var parsedItems: [Item]?
+
         //try all parsers until one succeeds
         for parser in self.parsers {
             do {
-                parsedItems = try parser.parse(input: code)
+                parsedItems = try parser.parse(input: data)
                 //we've parsed our items = nothing to do here more
                 if (parsedItems!.isEmpty == false) {
                     break;
@@ -62,6 +78,21 @@ class ParserService {
         //notify delegate, parsing finished
         DispatchQueue.main.async {
             self.delegate?.didFinishParsing(result: result)
+        }
+    }
+
+    private func fetchCodeFromUrl(scannedCode: String) throws -> String? {
+        guard let url = URL(string: scannedCode) else {
+            //not an url
+            return nil
+        }
+        print("got URL: \(url)")
+        do {
+            let data = try Data(contentsOf: url)
+            return String(data: data, encoding: String.Encoding.utf8)!
+        } catch {
+            print("failed to download data: \(error)")
+            throw error
         }
     }
 
